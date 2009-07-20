24b8a1a6d12382dc862cbb7e28618fb430eafb75
simonhalsey <simonhalsey@72c53ba6-7464-11de-927c-01e442f92a7c>
2009-07-20 19:11:23 +0000

    Added ID3 v1/v1.1 support

--- trunk/ID3TestBed/Form1.cs
+++ trunk/ID3TestBed/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -14,6 +15,47 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            System.Collections.ObjectModel.ReadOnlyCollection<string> G = MpegData.v1.Genre.GetGenres();
+            try
+            {
+                foreach (string item in Directory.GetFiles("C:\\Users\\Simon\\Desktop\\id3v1", "*.mp3"))
+                {
+                        sb.AppendLine(item);
+                    try
+                    {
+                        MpegData.v1.Tag myTag = MpegData.v1.Tag.GetTag(File.Open(item, FileMode.Open));
+
+                        if (myTag != null)
+                        {
+                            sb.AppendFormat("Title  : \"{0}\"{1}", myTag.SongTitle, Environment.NewLine);
+                            sb.AppendFormat("Artist : \"{0}\"{1}", myTag.Artist, Environment.NewLine);
+                            sb.AppendFormat("Album  : \"{0}\"{1}", myTag.Album, Environment.NewLine);
+                            sb.AppendFormat("Year   : \"{0}\"{1}", myTag.Year, Environment.NewLine);
+                            sb.AppendFormat("Comment: \"{0}\"{1}", myTag.Comment, Environment.NewLine);
+                            if (myTag.Track.HasValue)
+                                sb.AppendFormat("Track  : {0}{1}", myTag.Track, Environment.NewLine);
+                            sb.AppendFormat("Genre  : {0} ({1})", myTag.Genre, myTag.Genre < G.Count ? G[myTag.Genre] : "?");
+                        }
+                    }
+                    catch (MpegData.ID3Exception)
+                    {
+                        sb.AppendLine("Error parsing file.");
+                    }
+
+                    sb.AppendLine();
+                    sb.AppendLine();
+                }
+            }
+            catch(Exception ex)
+            {
+                sb.AppendLine(ex.Message);
+            }
+                textBox1.Text = sb.ToString();
         }
     }
 }
---
+++ trunk/MpegData/ID3Exception.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MpegData
+{
+    public class ID3Exception : Exception
+    {
+        public ID3Exception()
+            : base()
+        { }
+
+        public ID3Exception(string message)
+            : base(message)
+        { }
+
+        public ID3Exception(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
